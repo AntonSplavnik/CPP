c7fa2bc2d7b984e387ba6f44debc929ca1967f87
LazyPixel <antonsplavnik@gmail.com>
2025-03-26 00:09:04 +0100

    cpp01 ex04 looks finished, doble check if something is missing

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asplavni <asplavni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: antonsplavnik <antonsplavnik@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 19:18:49 by asplavni          #+#    #+#             */
-/*   Updated: 2025/03/24 19:46:10 by asplavni         ###   ########.fr       */
+/*   Updated: 2025/03/26 00:06:17 by antonsplavn      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,62 @@
 #include <string>
 #include <fstream>
 
+std::string replaceString(std::string const &fromFile, std::string const &string1, std::string const &string2) {
+
+	std::string toFile;
+
+	for (size_t i = 0; i <= fromFile.length() - string1.length();) {
+
+		if (fromFile[i] == string1[0] &&
+			fromFile[i + (string1.length() - 1)] == string1[string1.length() - 1] &&
+			fromFile.compare(i, string1.length(), string1) == 0) {
+
+			toFile += string2;
+			i += string1.length();
+		}
+		else{
+			toFile += fromFile[i];
+			i++;
+		}
+	}
+
+	toFile += fromFile.substr(fromFile.length() - string1.length());
+	return (toFile);
+}
+
 int main (int ac, char **av) {
 
 	if (ac != 4) {
 		std::cout << "number of arguments is incorrect" << std::endl;
 		return (1);
 	}
-	std::string fileName, string1, string2, fromFile;
+	std::string fileName = av[1];
+	std::string	string1 = av[2];
+	std::string	string2 = av[3];
+	std::string fromFile, toFile, line;
 
-	fileName = av[1];
-	string1 = av[2];
-	string2 = av[3];
+	std::ifstream reading(fileName);
 
-	std::ifstream file(fileName);
-
-	if (!file) {
-		std::cerr << "Eaild to open file." << std::endl;
+	if (!reading) {
+		std::cerr << "Faild to open file." << std::endl;
 		return (1);
 	}
 
-	while (std::getline(file, fromFile))
-		std:: cout << fromFile << std :: endl;
-
-	for (size_t i; i < fromFile.length(); i++) {
-
+	while (std::getline(reading, line)){
+		fromFile += line;
+		fromFile += "\n";
 	}
+
+	toFile += replaceString(fromFile, string1, string2);
+
+	std::ofstream writing(fileName + ".replace");
+
+	if (!writing) {
+		std::cerr << "Failed to open file for writing." << std::endl;
+		return 1;
+	}
+
+	writing << toFile;
+	writing.close();
 	return (0);
 }
